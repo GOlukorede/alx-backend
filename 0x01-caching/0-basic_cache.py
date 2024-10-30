40c98ee1824e4ba8919ce2bcf655c80fde321278
GOlukorede <samsongreats@gmail.com>
2024-10-30 21:08:47 +0100

    updated solution

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,10 @@ def put(self, key, item):
 Must assign to the dictionary self.cache_data the
 item value for the key key
 If key or item is None, this method should not do
-anything
+anything.
 def get(self, key):
 Must return the value in self.cache_data linked
-to key
+to key.
 If key is None or if the key doesn’t exist in
 self.cache_data, return None.
 """
