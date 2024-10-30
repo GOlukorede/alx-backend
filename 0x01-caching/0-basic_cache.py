d79079b21c167d4e6e6b94d05b699b91ddb00ae9
GOlukorede <samsongreats@gmail.com>
2024-10-30 20:55:12 +0100

    made the file executable

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,12 +7,12 @@ the parent class BaseCaching
 This caching system doesn’t have limit
 def put(self, key, item):
 Must assign to the dictionary self.cache_data the
-item value for the key key.
+item value for the key key
 If key or item is None, this method should not do
-anything.
+anything
 def get(self, key):
 Must return the value in self.cache_data linked
-to key.
+to key
 If key is None or if the key doesn’t exist in
 self.cache_data, return None.
 """
